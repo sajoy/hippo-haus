974c7f090ecc8c332ebab458656ad94f0a576de8
Christian Danielsen <ckdanielsen@gmail.com>
2015-11-12 20:28:51 -0800

    update generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 var responses = ["It is so obvious you need HIPPO!",
                  "You have obviously not adequately prepared for this party. You need HIPPO now!",
-                 "Your party is weak; get cool with HIPPO!"
+                 "Your party is weak; get cool with HIPPO!",
+                 "HIPPO IS WHAT YOU ARE NEEDING AT THIS TIME",
+                 "You are needing deiner hippo upgrade version 4",
+                 "Your party... it needs hippo"
                ];
 
 function getRandomInt(min, max) {
@@ -10,6 +13,6 @@ function getRandomInt(min, max) {
 $(function() {
   $("form").on("submit", function(e) {
     e.preventDefault();
-    $(".result").text(responses[getRandomInt(0,2)]);
+    $(".result").text(responses[getRandomInt(0,5)]);
   });
 });
