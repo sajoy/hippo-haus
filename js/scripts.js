57bc60974451d31754e42005b6cc57f83c7757d9
Christian Danielsen <ckdanielsen@gmail.com>
2015-11-12 20:26:30 -0800

    add random response generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
+var responses = ["It is so obvious you need HIPPO!",
+                 "You have obviously not adequately prepared for this party. You need HIPPO now!",
+                 "Your party is weak; get cool with HIPPO!"
+               ];
+
+function getRandomInt(min, max) {
+ return Math.floor(Math.random() * (max - min)) + min;
+}
+
 $(function() {
   $("form").on("submit", function(e) {
     e.preventDefault();
-
-    $(".result").text("You have obviously not adequately prepared for this party. You need HIPPO now!");
+    $(".result").text(responses[getRandomInt(0,2)]);
   });
 });
