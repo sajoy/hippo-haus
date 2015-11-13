06c790417985c251dc4d7961a503b8eda7c6a2c8
Nick Bernard <nick@nrbernard.com>
2015-11-12 20:15:25 -0800

    Add a form question

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ $(function() {
   $("form").on("submit", function(e) {
     e.preventDefault();
 
-    $(".result").text("It is obvious that you need Hippo!");
+    $(".result").text("You have obviously not adequately prepared for this party. You need HIPPO now!");
   });
 });
